4ed2814461c003effe23f16988b4a7786fac7c34
micha vie <micha@freeiam.com>
2022-10-13 13:32:31 +0200

    update nav

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -7,7 +7,7 @@ module.exports = {
       {
         title: 'General',
         collapsable: false,
-        children: prefix('general', ['overview', 'philosophy']),
+        children: prefix('general', ['overview', 'philosophy', 'faq', 'support']),
       },
       {
         title: 'Basics',
